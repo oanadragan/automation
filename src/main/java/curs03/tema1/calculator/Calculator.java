3f51e1cdb01640cade8d2b4166aaf05609ec1c4a
oanadragan <oanadragan17@gmail.com>
2022-10-14 21:44:42 +0300

    Java - Tema1, sapt3

---
+++ src/main/java/curs03/tema1/calculator/Calculator.java
@@ -0,0 +1,39 @@
+package curs03.tema1.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        if (args.length == 3) {
+            float a = Float.parseFloat(args[0]);
+            float b = Float.parseFloat(args[2]);
+            String operator = args[1];
+
+            switch (operator) {
+
+                case "+": {
+                    float result = a + b;
+                    System.out.println(a + " + " + b + " = " + result);
+                    break;
+                }
+                case "-": {
+                    float result = a - b;
+                    System.out.println(a + " - " + b + " = " + result);
+                    break;
+                }
+                case "*": {
+                    float result = a * b;
+                    System.out.println(a + " - " + b + " = " + result);
+                    break;
+                }
+                case "/": {
+                    float result = a / b;
+                    System.out.println(a + " - " + b + " = " + result);
+                    break;
+                }
+                default: {
+                    System.out.println("Invalid operator!");
+                    break;
+                }
+            }
+        }
+    }
+}
