81c9eb4083651b8c6a75c09a7f33e777fdba0e5e
oanadragan <oanadragan17@gmail.com>
2022-10-17 09:55:14 +0300

    Java - Sapt3, Tema2

--- src/main/java/curs03/tema1/calculator/February.java
+++ src/main/java/curs03/tema1/calculator/February.java
@@ -1,16 +1,19 @@
 package curs03.tema1.calculator;
-
+import java.util.Scanner;
 public class February {
     public static void main(String[] args) {
-
-        for (int year = 1900; year <= 2016; year++) {
-            if (((year % 4 == 0) && (year % 100 != 0)) || ((year % 100 == 0) && (year % 400 != 0)))
-            {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input a year: ");
+        int year = input.nextInt();
+        if (year >= 1900 && year <= 2016) {
+            if (((year % 4 == 0) && (year % 100 != 0)) || ((year % 100 == 0) && (year % 400 != 0))) {
                 System.out.println("In " + year + " February had 29 days");
-            }
-            else {
+            } else {
                 System.out.println("In " + year + " February had 28 days");
             }
+        } else {
+            System.out.println("Please enter a year between 1900 and 2016");
         }
     }
 }
+
